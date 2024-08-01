75afcae71968a9bcbe2efabef79874ca132edcce
supriya <supriyarkt1999@gmail.com>
2024-08-01 13:26:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { toast, ToastContainer } from "react-toastify";
+import { LC, NC, SC, UC } from "./Data/Passchar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   let [upercase, setUperCase] = useState(false);
+   let [lowercase, setLowercase] = useState(false);
+   let [numbers, setNumbers] = useState(false);
+   let [symbols, setSymbols] = useState(false);
+   let [plength, setPlength] = useState(10);
+   let [password, setPassword] = useState("");
+
+   const generatePassword = () => {
+      if (upercase || lowercase || numbers || symbols) {
+         let finalPassword = "";
+         let charSet = "";
+         if (upercase) charSet += UC;
+         if (lowercase) charSet += LC;
+         if (numbers) charSet += NC;
+         if (symbols) charSet += SC;
+         for (let i = 0; i < plength; i++) {
+            finalPassword += charSet.charAt(
+               Math.floor(Math.random() * charSet.length)
+            );
+         }
+
+         toast.success("Password Generated Successfully...");
+         setPassword(finalPassword);
+      } else {
+         toast.error("Please Select One Checkbox...");
+      }
+   };
+   let CopyPassword = () => {
+       if(password.length >5){
+          navigator.clipboard.writeText(password);
+          toast.info("Password Copied Successfully...");
+          document.getElementById("btn").style.backgroundColor = "green";
+       }
+       else{
+          toast.error("Please Genrate the password...");
+       }
+   };
+  
+
+   return (
+      <>
+         <div className="App">
+            <ToastContainer />
+            <h2>Password Genrator</h2>
+            <div className="password-div">
+               <input
+                  type="text"
+                  readOnly
+                  value={password}
+                  onChange={() => setPassword()}
+               />
+               <button id="btn" onClick={CopyPassword}>Copy</button>
+            </div>
+            <div className="passwordBox-lable">
+               <label htmlFor="" value={plength}>
+                  {plength}
+               </label>
+               <input
+                  type="range"
+                  max={20}
+                  min={7}
+                  name="range"
+                  value={plength}
+                  onChange={(e) => setPlength(e.target.value)}
+               />
+            </div>
+            <div className="upercase normal-div">
+               <label htmlFor="">Include UperCase latter</label>
+               <input
+                  type="checkbox"
+                  name="upper"
+                  checked={upercase}
+                  onChange={() => setUperCase(!upercase)}
+               />
+            </div>
+            <div className="lowcase normal-div">
+               <label htmlFor="">Include LowerCase latter</label>
+               <input
+                  type="checkbox"
+                  name="lower"
+                  checked={lowercase}
+                  onChange={() => setLowercase(!lowercase)}
+               />
+            </div>
+            <div className="numbers normal-div">
+               <label htmlFor="">Include Numbers</label>
+               <input
+                  type="checkbox"
+                  name="numbers"
+                  checked={numbers}
+                  onChange={() => setNumbers(!numbers)}
+               />
+            </div>
+            <div className="symbols normal-div">
+               <label htmlFor="">Include Symbols</label>
+               <input
+                  type="checkbox"
+                  name="symbols"
+                  checked={symbols}
+                  onChange={() => setSymbols(!symbols)}
+               />
+            </div>
+            <div className="btn-gen normal-div">
+               <button onClick={generatePassword}>Generate Password</button>
+            </div>
+         </div>
+      </>
+   );
 }
 
 export default App;
---
+++ src/Data/Passchar.jsx
@@ -0,0 +1,4 @@
+export let UC = "ABCEFGHIJKLMNOPQRSTUVWXYZ";
+export let LC = "abcdefghijklmnopqrstuvwxyz";
+export let SC = '!"#$%&()*+,-./:;<=>?@[]^_`{|}';
+export let NC= "0123456789";
